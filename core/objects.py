9a2333de2254a6bb26db2331c80f96e85fae62d8
MJ-GITHUB-007 <junaid1607@outlook.com>
2023-11-26 19:38:23 +0530

    Updated file

--- core/objects.py
+++ core/objects.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 from core.constants import WHITE
@@ -34,8 +36,8 @@ class Ball:
         self.x = self.original_X = x
         self.y = self.original_Y = y
         self.radius = radius
-        self.x_vel = self.MAX_VEL
-        self.y_vel = 0
+        self.x_vel = self.MAX_VEL * random.choice([-1, 1])
+        self.y_vel = random.uniform(-3, 3)
     
     def draw(self, win):
         pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
@@ -47,8 +49,8 @@ class Ball:
     def reset(self):
         self.x = self.original_X
         self.y = self.original_Y
-        self.y_vel = 0
-        self.x_vel *= -1
+        self.x_vel = self.MAX_VEL * random.choice([-1, 1])
+        self.y_vel = random.uniform(-3, 3)
 
 if __name__ == '__main__':
     pass
